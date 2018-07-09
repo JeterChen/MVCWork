cac22a9cf0541078c8846d79cc61d46f036ac6c5
demofan <demo@demo.tc>
2018-07-09 17:03:01 +0800

    調整 HemoService

--- MVCHomeWord/MoneyMVC/Controllers/HomeController.cs
+++ MVCHomeWord/MoneyMVC/Controllers/HomeController.cs
@@ -20,11 +20,9 @@ namespace MoneyMVC.Controllers
         public ActionResult SubAction()
         {
             
-            var result =new HomeService();
+            var result =new HomeService().LookupAllData();
 
-            ViewData["Data"] = result;
-
-            return View();
+            return View(result);
         }
 
         public ActionResult About()
